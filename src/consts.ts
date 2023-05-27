626b703ad2930fd352fa3e8d2259df3261cade65
raf <rraf@tuta.io>
2023-05-27 10:45:31 +0100

    update: added website info

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,3 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Raf Shahid - Portfolio - ';
+export const SITE_DESCRIPTION = 'An archive of my many interests';
+export const HOMEPAGE_URL = 'https://raafay.world'
